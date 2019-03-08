b8c112f7c17ab6d4a53726bb1af8ef6f4dac15e1
Anupom Roy <anupomr@yahoo.com>
2019-03-07 22:01:07 -0500

    Create Java class to consume REST API

---
+++ app/src/main/java/app/anupom/utility/BackgroundWorker.java
@@ -0,0 +1,10 @@
+package app.anupom.utility;
+
+import android.os.AsyncTask;
+
+public class BackgroundWorker extends AsyncTask<Void,Void,Void> {
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }//
+}
