619cd6d2bdde1ed8169d1a5a62190471d5085e6e
Anupom Roy <anupomr@yahoo.com>
2019-03-21 14:19:11 -0400

    Add menu in Books_Details

--- app/src/main/java/com/example/anupo/softproject2application/BooksDetailsActivity.java
+++ app/src/main/java/com/example/anupo/softproject2application/BooksDetailsActivity.java
@@ -3,16 +3,32 @@ package com.example.anupo.softproject2application;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BooksDetailsActivity extends AppCompatActivity {
     private TextView tvTitle, tvDesc, tvId,tvPrice,tvEdition, tvIsbn, tvPublisher;
     private ImageView img;
+    Button checkout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books_details);
+        checkout=findViewById(R.id.btCheckout);
+        checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent checkoutIntent=new Intent(BooksDetailsActivity.this, PaymentActivity.class);
+                startActivity(checkoutIntent);
+            }
+        });
+
         tvId=(TextView)findViewById(R.id.tvId);
         tvTitle=(TextView)findViewById(R.id.tvTitle);
         tvDesc=(TextView)findViewById(R.id.tvDesc);
@@ -30,4 +46,46 @@ public class BooksDetailsActivity extends AppCompatActivity {
 
         img.setImageResource(intent.getExtras().getInt("thumbnail"));
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.home:
+                Intent homeIntent=new Intent(this,MainActivity.class);
+                startActivity(homeIntent);
+                //Toast.makeText(this, "You selected start!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.login:
+                Intent loginIntent=new Intent(this,LoginActivity.class);
+                startActivity(loginIntent);
+                Toast.makeText(this, "You selected login!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.book:
+                Intent books=new Intent(this,BooksActivity.class);
+                startActivity(books);
+                //Toast.makeText(this, "You selected book!", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.bookList:
+                Intent booksListIntent=new Intent(this,BooksListActivity.class);
+                startActivity(booksListIntent);
+
+                break;
+            case R.id.exit:
+                Toast.makeText(this, "You selected exit!", Toast.LENGTH_LONG).show();
+                System.exit(1);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
