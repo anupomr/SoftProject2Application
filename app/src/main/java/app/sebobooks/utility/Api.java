5d9c85773b79bcd2fc8f7ed1b7e9e05a0a63a142
Anupom Roy <anupomr@yahoo.com>
2019-04-01 11:39:40 -0400

    Create a call to add user

--- app/src/main/java/app/sebobooks/utility/Api.java
+++ app/src/main/java/app/sebobooks/utility/Api.java
@@ -1,5 +1,18 @@
 package app.sebobooks.utility;
 
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.POST;
+
 // Define All API Calls
 public interface Api {
+
+    @POST("client")
+    Call<ResponseBody> createUser(
+            @Field("email") String email,
+            @Field("password") String password,
+            @Field("name") String name,
+            @Field("school") String school
+    );
 }
