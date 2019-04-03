425d407eb6536130eef86f149d1979a6a238375f
Anupom Roy <anupomr@yahoo.com>
2019-04-02 22:52:53 -0400

    pass unit test case

--- app/src/androidTest/java/com/example/anupo/softproject2application/MainActivityTest.java
+++ app/src/androidTest/java/com/example/anupo/softproject2application/MainActivityTest.java
@@ -1,5 +1,8 @@
 package com.example.anupo.softproject2application;
 
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -10,20 +13,24 @@ import static org.junit.Assert.*;
 public class MainActivityTest {
 
     @Rule
-    public  ActivityTestRule<MainActivity>  mActivityTestRule =new ActivityTestRule<MainActivity>(MainActivity.class);
+    public ActivityTestRule<MainActivity> mActivityTestRule =new ActivityTestRule<MainActivity>(MainActivity.class);
 
+    private MainActivity mainActivity=null;
 
     @Before
     public void setUp() throws Exception {
+        mainActivity=mActivityTestRule.getActivity();
     }
 
 
     @Test
     public void testLaunch()
     {
-
+        View view=mainActivity.findViewById(R.id.buttonLogin);
+        assertNotNull(view);
     }
     @After
     public void tearDown() throws Exception {
+        mainActivity=null;
     }
 }
