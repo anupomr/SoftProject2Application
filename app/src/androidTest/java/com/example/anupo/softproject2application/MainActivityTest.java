ac9e6ace4e2e6fc2cff8eae14a8e11f1010570ba
Anupom Roy <anupomr@yahoo.com>
2019-04-02 22:17:31 -0400

    Fix Gradle and write unit testing

---
+++ app/src/androidTest/java/com/example/anupo/softproject2application/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.example.anupo.softproject2application;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public  ActivityTestRule<MainActivity>  mActivityTestRule =new ActivityTestRule<MainActivity>(MainActivity.class);
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+
+    @Test
+    public void testLaunch()
+    {
+
+    }
+    @After
+    public void tearDown() throws Exception {
+    }
+}
